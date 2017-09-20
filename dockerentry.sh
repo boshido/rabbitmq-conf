4b48873ad21ea459bd39d8d21739b97699f6920d
Watchrapong Agsonchu <boshido@windowslive.com>
2017-09-20 13:24:06 +0700

    Modify rabbitmq.tmpl to be able to load rabbitmq_definitions and modify dockerentry to support overiding rabbitmq_definitions

--- dockerentry.sh
+++ dockerentry.sh
@@ -2,9 +2,14 @@
 
 cp /run.sh /opt/rancher/bin/
 
-if [[ -v ALTERNATE_CONF ]]; then 
-    echo "Custom template found: overriding internal template";
-    printenv ALTERNATE_CONF > /etc/confd/templates/rabbitmq.tmpl; 
+if [ ! -z ${ALTERNATE_CONF+x} ]; then
+    echo "Custom template of rabbitmq found: overriding internal template";
+    printenv ALTERNATE_CONF > /etc/confd/templates/rabbitmq.tmpl;
+fi
+
+if [ ! -z ${ALTERNATE_DEF_CONF+x} ]; then
+    echo "Custom template of rabbitmq_definitions found: overriding internal template";
+    printenv ALTERNATE_DEF_CONF > /etc/confd/templates/rabbitmq_definitions.tmpl;
 fi
 
 exec /confd $@ $CONFD_ARGS
